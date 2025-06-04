01bd9e1adfe1ac9d9b50b6f130327311ce4610d7
Fdeinparvar <37465055+Fdeinparvar@users.noreply.github.com>
2025-06-04 13:19:44 -0500

    Delete 02-receiptstatusaverage.sql

--- 02-receiptstatusaverage.sql
+++
@@ -1,8 +0,0 @@
---When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
-SELECT A.rewards_receipt_status AS Receipt_Status, A.Average_Spent AS Average_Spent, CASE A.r WHEN 1 THEN CONCAT(A.rewards_receipt_status , ' reciepts have the higher average spent per receipt') ELSE '' END AS Result FROM       
-            --Subquery returns the average spent for rewards receipt status that are accepted or rejected. It orders them by average spent    
-            (SELECT ROW_NUMBER() AS r, rewards_receipt_status, AVG(total_spent) AS Average_Spent FROM receipt 
-            WHERE rewards_receipt_status IN ('Accepted','Rejected')
-            GROUP BY rewards_receipt_status
-            ORDER BY AVG(total_spent)
-            ) A
